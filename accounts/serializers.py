2f0bfe28c5807f1dea03e89b1d7433cd5b300e82
Yashodhan Singh <yashodhan.singh@unoapp.io>
2020-02-02 02:05:50 +0530

    improvement: `UserListSerializer`
    - add password field (write-only/not-required)
    - add is_active field

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 
@@ -11,6 +12,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
 User = get_user_model()
 class UserListSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
     groups = GroupSerializer(many=True, read_only=True)
 
     class Meta:
@@ -22,5 +24,11 @@ class UserListSerializer(serializers.ModelSerializer):
             'username',
             'is_superuser',
             'is_staff',
+            'is_active',
             'groups',
+            'password'
         )
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserListSerializer, self).create(validated_data)
