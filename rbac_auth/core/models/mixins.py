5b3a3016d4126b5c4428ffafb4667e478ed53430
Yashodhan Singh <yashodhan.singh@unoapp.io>
2020-02-10 00:58:08 +0530

    add slug model mixin  and archivable model mixin

--- rbac_auth/core/models/mixins.py
+++ rbac_auth/core/models/mixins.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
+from rbac_auth.contrib.rest_framework.mixins import DestroyModelMixin
 
 
 class TimestampMixin(models.Model):
@@ -8,3 +10,21 @@ class TimestampMixin(models.Model):
 
     class Meta:
         abstract = True
+
+class ArchivableModelMixin(DestroyModelMixin, models.Model):
+    archived = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class SluggedModelMixin(models.Model):
+    __slugged_field = 'name'
+    slug = models.SlugField(unique=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        slugged = getattr(self, self.__slugged_field)
+        self.slug = slugify(slugged)
+        super(SluggedModelMixin, self).save(*args, **kwargs)
